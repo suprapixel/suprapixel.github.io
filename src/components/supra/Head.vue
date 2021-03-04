31e189edb8be453cf56aa5643c56f3ff88cdae6c
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-03-04 09:16:34 -0300

    Created head component

---
+++ src/components/supra/Head.vue
@@ -0,0 +1,29 @@
+<template>
+	<span v-if="false"></span>
+</template>
+
+<script>
+export default {
+	head() {
+		const isDark = this.$colorMode.value === 'dark'
+		return {
+			link: [
+				{
+					rel: 'icon',
+					type: 'image/svg+xml',
+					href: isDark ? '/favicon-dark.svg' : '/favicon.svg',
+				},
+				{
+					rel: 'alternate icon',
+					href: '/favicon.ico',
+				},
+				{
+					rel: 'mask-icon',
+					href: '/safari-pinned-tab.svg',
+					color: '#272B36',
+				},
+			],
+		}
+	},
+}
+</script>
